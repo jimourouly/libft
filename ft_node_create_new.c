90bee83e88d03d98d9f9a39840fd66c8dd33869c
Jimmy Roulet <jroulet@c1r13s1.42lausanne.ch>
2023-12-09 16:46:52 +0100

    added protection malloc

--- ft_node_create_new.c
+++ ft_node_create_new.c
@@ -6,7 +6,7 @@
 /*   By: jroulet <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 13:57:58 by jroulet           #+#    #+#             */
-/*   Updated: 2023/12/09 16:30:09 by jroulet          ###   ########.fr       */
+/*   Updated: 2023/12/09 16:46:23 by jroulet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ t_node	*ft_node_create_new(int value)
 	t_node	*result;
 
 	result = malloc(sizeof(t_node));
+	if (!result)
+		return (NULL);
 	result->value = value;
 	result->next = NULL;
 	return (result);
