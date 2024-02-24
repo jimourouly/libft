65e3300b70276ec9f317e9f6fc55397b9e753161
jroulet <jroulet@c2r5s1.42lausanne.ch>
2024-02-24 18:47:24 +0100

    ft_strcpy.c norminetted

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,17 +6,17 @@
 /*   By: jroulet <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 13:09:37 by jroulet           #+#    #+#             */
-/*   Updated: 2024/02/24 13:09:39 by jroulet          ###   ########.fr       */
+/*   Updated: 2024/02/24 17:12:59 by jroulet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_strcpy(char *dst, const char *src)
+void	ft_strcpy(char *dst, const char *src)
 {
-    while (*src)
-    {
-        *dst = *src;
-        dst++;
-        src++;
-    }
-    *dst = '\0'; // Don't forget to add the null terminator
+	while (*src)
+	{
+		*dst = *src;
+		dst++;
+		src++;
+	}
+	*dst = '\0';
 }
